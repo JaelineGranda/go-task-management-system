9b67e8b4c1a6967655fd44ba3a3914d4e13d05f6
Jaeline.Granda <jaeline.granda@nielsen.com>
2024-05-24 18:11:42 -0400

    set up routes and start server

---
+++ task-management-system/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"task-management-system/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/tasks", handlers.CreateTask)
+	router.GET("/tasks/:id", handlers.ReadTask)
+	router.PUT("/tasks/:id", handlers.UpdateTask)
+	router.DELETE("/tasks/:id", handlers.DeleteTask)
+	router.GET("/tasks", handlers.ListTasks)
+
+	router.Run(":8080")
+}
